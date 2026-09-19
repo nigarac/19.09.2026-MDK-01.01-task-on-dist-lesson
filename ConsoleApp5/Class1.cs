d8fb3c1b6c2d8d1ec2a19aaf22a8e931a5cb4b8d
нигапак <@DESKTOP-82TCB8S>
2026-09-19 13:50:56 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public  class Parent
+    {
+        public string number { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+        public string doo {  get; set; }
+
+        public virtual void WarningSound()
+        {
+            Console.WriteLine("Устройство подаёт сигнал");
+        }
+    }
+
+    public class Thermometer : Parent
+    {
+        private double temp { get; set; }
+
+        public void TempSearch()
+        {
+            temp = new Random().Next(32, 40);
+            Console.WriteLine($"Ваша температура = {temp}");
+        }
+
+        public override void WarningSound()
+        {
+            Console.WriteLine("пип-пип-пип");
+        }
+    }
+
+    public class Weight : Parent
+    {
+        private double weight { get; set; }
+
+        public void WeightSearch()
+        {
+            weight = new Random().Next(50, 75);
+            Console.WriteLine($"Ваш вес = {weight}");
+        }
+        public override void WarningSound()
+        {
+            Console.WriteLine("туру-туру-туру-туру");
+        }
+    }
+
+    public class Barometr : Parent
+    {
+        private double bars { get; set; }
+
+        public void BarSearch()
+        {
+            bars = new Random().Next(50, 75);
+            Console.WriteLine($"Давление = {bars}");
+        }
+        public override void WarningSound()
+        {
+            Console.WriteLine("бам-бом-бам-бом");
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp5;
+
+Parent parent = new Parent();
+Thermometer thermometer = new Thermometer();
+Weight weight = new Weight();
+Barometr barometr = new Barometr();
+
+Console.WriteLine("Введите Инв.Номер");
+parent.number = Console.ReadLine();
+
+Console.WriteLine("Введите название");
+parent.name = Console.ReadLine();
+
+Console.WriteLine("Введите цвет");
+parent.color = Console.ReadLine();
+
+Console.WriteLine("Введите исполнение");
+parent.doo = Console.ReadLine();
+
+thermometer.WarningSound();
+thermometer.TempSearch();
+
+weight.WarningSound();
+weight.WeightSearch();
+
+
+barometr.WarningSound();
+barometr.BarSearch();
